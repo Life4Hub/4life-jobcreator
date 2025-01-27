55c46053356d655c3896d17088cfa6922ff460dc
Life4Hub <johannesfleck97@icloud.com>
2025-01-27 14:12:22 +0100

    TEST

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,6 @@ author 'Life4Tune'
 description 'Job Creator Skript'
 version '1.0.0'
 
-resource_type 'gametype' { name = 'gta' }
+resource_type 'gametype' { name = '' }
 
 client_script 'client.lua'
