8853cfa11a94bd84b8028ea00e7616133b3fc587
Life4Hub <johannesfleck97@icloud.com>
2025-01-27 14:05:27 +0100

    Manifest

--- config.lua
+++ config.lua
@@ -24,8 +24,69 @@ Config.Jobs = {
             [4] = 'Captain',
             [5] = 'Chief'
         }
+    },
+    ['mechanic'] = {
+        label = 'Mechanic',
+        grades = {
+            [0] = 'Apprentice',
+            [1] = 'Technician',
+            [2] = 'Senior Technician',
+            [3] = 'Supervisor',
+            [4] = 'Manager',
+            [5] = 'Chief Mechanic'
+        }
+    },
+    ['taxi'] = {
+        label = 'Taxi',
+        grades = {
+            [0] = 'Trainee',
+            [1] = 'Driver',
+            [2] = 'Senior Driver',
+            [3] = 'Supervisor',
+            [4] = 'Manager',
+            [5] = 'Chief'
+        }
+    },
+    ['firefighter'] = {
+        label = 'Firefighter',
+        grades = {
+            [0] = 'Trainee',
+            [1] = 'Firefighter',
+            [2] = 'Senior Firefighter',
+            [3] = 'Lieutenant',
+            [4] = 'Captain',
+            [5] = 'Chief'
+        }
+    },
+    ['teacher'] = {
+        label = 'Teacher',
+        grades = {
+            [0] = 'Assistant',
+            [1] = 'Teacher',
+            [2] = 'Senior Teacher',
+            [3] = 'Head of Department',
+            [4] = 'Deputy Principal',
+            [5] = 'Principal'
+        }
     }
 }
+
 Config.MenuCommand = 'jobcreator'
 
 Config.Locale = 'en'
+
+function Config.getJobLabel(jobName)
+    if Config.Jobs[jobName] then
+        return Config.Jobs[jobName].label
+    else
+        return nil
+    end
+end
+
+function Config.getJobGradeLabel(jobName, grade)
+    if Config.Jobs[jobName] and Config.Jobs[jobName].grades[grade] then
+        return Config.Jobs[jobName].grades[grade]
+    else
+        return nil
+    end
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Life4Tune'
+description 'Job Creator Skript'
+version '1.0.0'
+
+resource_type 'gametype' { name = 'gta' }
+
+client_script 'client.lua'
